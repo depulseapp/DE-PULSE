a8cf5f4e818609d191f977da846be31203d76f06
depulseapp <de.pulse.app@gmail.com>
2026-08-19 22:40:41 -0700

    docs: close v18.8.0 release identity and certification

--- app_bootstrap.go
+++ app_bootstrap.go
@@ -17,6 +17,6 @@ var finnhubAPIBaseURL = "https://finnhub.io/api/v1"
 var finnhubMinRequestInterval = 1100 * time.Millisecond
 
 const appName = "DE.PULSE"
-const appVersion = "18.7.0"
+const appVersion = "18.8.0"
 const releaseChannel = "STABLE"
-const buildID = "v18.7.0-stable-20260819"
+const buildID = "v18.8.0-stable-20260819"
