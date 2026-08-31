71d58b8020b0b32438da5a745fa5f40843ae82fa
depulseapp <de.pulse.app@gmail.com>
2026-08-31 15:16:42 -0700

    fix(host013): bound Azure provider registration

--- governance/hosted-infrastructure/azure/versions.tf
+++ governance/hosted-infrastructure/azure/versions.tf
@@ -11,6 +11,7 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
+  subscription_id                 = var.subscription_id
+  tenant_id                       = var.tenant_id
+  resource_provider_registrations = "none"
 }
